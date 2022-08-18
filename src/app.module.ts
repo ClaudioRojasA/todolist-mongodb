8da507c2aae35a7c4256d4d71c2e3bd70c62bd35
Claude Rojas <rojasamado.claudio@gmail.com>
2022-08-18 12:44:20 -0400

    Todo Resources created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/todo')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/todo'), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
